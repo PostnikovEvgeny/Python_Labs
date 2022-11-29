68d68d44b5d30c72ca4ba55813cedf7412683ca9
Постников Евгений Рис-21-1б <92161003+PostnikovEvgeny@users.noreply.github.com>
2022-11-29 17:11:51 +0500

    Добавлено нахождение среднего значения

--- PythonLab3.py
+++ PythonLab3.py
@@ -7,3 +7,8 @@ pyDatalog.create_terms('X, Y, Result, Sum, Average, Range, RandSum, Median, Rand
 print(f"Сумма элементов ряда 0 - {number}:\n")
 (Sum[Range] == sum_(X, for_each = X)) <= X.in_(range_(Range + 1))
 print(f"{Sum == Sum[number]}\n")
+
+print(f"Среднее значение:\n")
+(Average[Range] == Result) <= (Range / 2 == Result)
+print(f"{Average == Average[number]}\n")
+
