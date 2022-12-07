37fe27d5c62ba54b80596765711144e65f5ff2c2
Постников Евгений Рис-21-1б <92161003+PostnikovEvgeny@users.noreply.github.com>
2022-12-07 11:26:24 +0500

    Исправлена кодировка

--- PythonLab3.py
+++ PythonLab3.py
@@ -1,25 +1,25 @@
-#-*- coding: cp1251 -*-
+#-*- coding:utf-8*-
 from random import randint
 from pyDatalog import pyDatalog
 number = 10
 pyDatalog.create_terms('X, Y, Result, Sum, Average, Range, RandSum, Median, Rand')
 
-print(f"Сумма элементов ряда 0 - {number}:\n")
+print(f"РЎСѓРјРјР° С‡РёСЃРµР» 0 - {number}:\n")
 (Sum[Range] == sum_(X, for_each = X)) <= X.in_(range_(Range + 1))
 print(f"{Sum == Sum[number]}\n")
 
-print(f"Среднее значение:\n")
+print(f"СЃСЂРµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ:\n")
 (Average[Range] == Result) <= (Range / 2 == Result)
 print(f"{Average == Average[number]}\n")
 
 randlist = [randint(0, 99) for _ in range(0, 100)]
 randlist.sort()
 
-print(f"Сумма 100 случайных значений:\n")
+print(f"СЃСѓРјРјР° 100 СЂР°РЅРґРѕРјРЅС‹С… РёРЅС‚РѕРІ:\n")
 (Sum[Rand] == sum_(X, for_each = X)) <= X.in_(Rand)
 print(f"{Sum[randlist] == RandSum}\n")
 
-print(f"Медиана:\n")
+print(f"РјРµРґРёР°РЅР°:\n")
 (Median[Rand] == Result) <= ((Rand[49] + Rand[50]) / 2 == Result)
 print(Median[randlist] == Median)
 
